c69c15b7c41ffc37a991161f290bab5db54fada1
kinfuy <Yonghu520@outlook.com>
2023-03-08 19:58:59 +0800

    fix: detail

--- packages/core/flow/flow.ts
+++ packages/core/flow/flow.ts
@@ -6,41 +6,58 @@ import { loadJsonFile } from '../fs/load';
 const flowConfigPath = `${join(STORE_ROOT, `flows.json`)}`;
 
 export interface FlowDesc {
-  path: string;
+  name: string;
   alias: string;
-  create: string;
+  desc: string;
 }
 
 export class FlowManage {
-  private _flows: Flow[] = [];
-  private _flow_index: FlowDesc[] = [];
+  private _flows: FlowDesc[] = [];
   constructor() {
     this.load();
   }
 
   async load() {
     const flows = await loadJsonFile<FlowDesc[]>(flowConfigPath).catch(() => {});
-    if (flows) this._flow_index = flows;
+    if (flows) this._flows = flows;
   }
 
-  find(name: string) {
-    return this._flows.find((x) => (x.alias = name));
+  check(name: string) {
+    return this._flows.some((x) => (x.alias = name));
+  }
+
+  async find(name: string): Promise<Flow | null> {
+    if (this.check(name)) {
+      const flow = await loadJsonFile<Flow>(join(STORE_ROOT, `${name}.json}`)).catch(() => {});
+      if (flow) return flow;
+      return null;
+    }
+    return null;
   }
 
   update(name: string, flow: Flow) {
-    this._flows.forEach((x) => {
-      if (x.alias === name) {
-        x.actions = flow.actions;
-        x.alias = flow.alias;
-        x.content = flow.content;
-        x.name = flow.name;
-        x.desc = flow.desc;
-      }
+    if (this.check(name)) {
+      this._flows.forEach((x) => {
+        if (x.alias === name) {
+          x.alias = flow.alias;
+        }
+      });
       this.storageSave();
-    });
+    }
   }
 
   delete() {}
-  add() {}
+  add(flow: Flow) {
+    if (this.check(flow.alias)) {
+      this.update(flow.alias, flow);
+    } else {
+      this._flows.push({
+        alias: flow.alias,
+        name: flow.name,
+        desc: flow.desc
+      });
+    }
+  }
+
   storageSave() {}
 }
